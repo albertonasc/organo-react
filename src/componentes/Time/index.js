6c435753bc9325c2dcf3bd3c0e6c9a6cf8d266cb
Alberto Nascimento <beto.bdn@gmail.com>
2023-09-26 16:15:33 -0300

    Escondendo times vazios e limpando o form após criar card

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -6,18 +6,20 @@ const Time = (props) => {
   const corBorda = { borderColor: props.corPrimaria };
 
   return (
-    <section className="time" style={corFundo}>
-      <h3 style={corBorda}>{props.nome}</h3>
-      <div className="colaboradores">
-        {props.colaboradores.map((colaborador) => (
-          <Colaborador
-            nome={colaborador.nome}
-            cargo={colaborador.cargo}
-            imagem={colaborador.imagem}
-          />
-        ))}
-      </div>
-    </section>
+    props.colaboradores.length > 0 && (
+      <section className="time" style={corFundo}>
+        <h3 style={corBorda}>{props.nome}</h3>
+        <div className="colaboradores">
+          {props.colaboradores.map((colaborador) => (
+            <Colaborador
+              nome={colaborador.nome}
+              cargo={colaborador.cargo}
+              imagem={colaborador.imagem}
+            />
+          ))}
+        </div>
+      </section>
+    )
   );
 };
 
