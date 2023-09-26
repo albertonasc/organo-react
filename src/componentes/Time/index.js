2f2e1d26fbc2fd0a0f4bc759e3463496d06a402e
Alberto Nascimento <beto.bdn@gmail.com>
2023-09-26 14:16:36 -0300

    Melhorando componente Time

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,9 +1,12 @@
 import "./Time.css";
 
 const Time = (props) => {
+  const corFundo = { backgroundColor: props.corSecundaria };
+  const corBorda = { borderColor: props.corPrimaria };
+
   return (
-    <section className="time">
-      <h3>{props.nome}</h3>
+    <section className="time" style={corFundo}>
+      <h3 style={corBorda}>{props.nome}</h3>
     </section>
   );
 };
